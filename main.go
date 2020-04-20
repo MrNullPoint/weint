1b570148b145ad65f134b5e2e221eb03dc095aed
mrnullpoint <598757315@qq.com>
2020-04-21 00:04:14 +0800

    Add: implement all output.go

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"Weint/weint"
+	"errors"
 	"github.com/urfave/cli"
 	"log"
 	"os"
@@ -16,14 +18,16 @@ func main() {
 				Required: true,
 			},
 			&cli.BoolFlag{
-				Name:    "info",
-				Aliases: []string{"i"},
-				Usage:   "set this flag means you can get weibo user profile",
+				Name:        "info",
+				Aliases:     []string{"i"},
+				DefaultText: "true",
+				Usage:       "set this flag means you can get weibo user profile",
 			},
 			&cli.BoolFlag{
-				Name:    "weibo",
-				Aliases: []string{"w"},
-				Usage:   "set this flag means you can get user weibo list",
+				Name:        "weibo",
+				Aliases:     []string{"w"},
+				DefaultText: "false",
+				Usage:       "set this flag means you can get user weibo list",
 			},
 			&cli.StringFlag{
 				Name:    "proxy",
@@ -31,11 +35,56 @@ func main() {
 				Usage:   "set proxy",
 			},
 			&cli.StringFlag{
-				Name:    "proxy",
-				Aliases: []string{"p"},
-				Usage:   "set proxy",
+				Name:    "out",
+				Aliases: []string{"o"},
+				Usage:   "set output type, csv/json/db/elastic",
+			},
+			&cli.StringFlag{
+				Name:    "file",
+				Aliases: []string{"f"},
+				Usage:   "set output filename",
+			},
+			&cli.StringFlag{
+				Name:        "host",
+				Aliases:     []string{"h"},
+				Usage:       "set elastic search address",
+				DefaultText: "127.0.0.1:9200",
 			},
 		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		spider := weint.NewSpider()
+		spider.Uid(c.String("user"))
+
+		if c.Bool("info") {
+			spider.Type(weint.TYPE_INFO)
+		}
+
+		if c.Bool("weibo") {
+			spider.Type(weint.TYPE_WEIBO)
+		}
+
+		var filename string
+		if c.String("file") == "" {
+			filename = ""
+		} else {
+			filename = c.String("file")
+		}
+
+		var out weint.OutInterface
+
+		switch c.String("out") {
+		case "csv":
+		case "json":
+		case "db":
+		case "elastic":
+			spider.Out(weint.ElasticOut{Host: c.String("host")})
+		default:
+			return errors.New("output type you input is not supported yet")
+		}
+
+		return spider.Run()
 	}
 
 	err := app.Run(os.Args)
--- weint/output.go
+++ weint/output.go
@@ -39,3 +39,35 @@ func (o *ConsoleOut) WriteWeiboInfo(info *WeiboInfo) error {
 	fmt.Println()
 	return nil
 }
+
+func (o *SQLiteOut) WriteUserInfo(info *UserInfo) error {
+	panic("implement me")
+}
+
+func (o *SQLiteOut) WriteWeiboInfo(info *WeiboInfo) error {
+	panic("implement me")
+}
+
+func (o *ElasticOut) WriteUserInfo(info *UserInfo) error {
+	panic("implement me")
+}
+
+func (o *ElasticOut) WriteWeiboInfo(info *WeiboInfo) error {
+	panic("implement me")
+}
+
+func (o *FileCSVOut) WriteUserInfo(info *UserInfo) error {
+	panic("implement me")
+}
+
+func (o *FileCSVOut) WriteWeiboInfo(info *WeiboInfo) error {
+	panic("implement me")
+}
+
+func (o *FileJsonOut) WriteUserInfo(info *UserInfo) error {
+	panic("implement me")
+}
+
+func (o *FileJsonOut) WriteWeiboInfo(info *WeiboInfo) error {
+	panic("implement me")
+}
