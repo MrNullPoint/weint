21ad7f72180f58c0791127b9182b244354dadc19
mrnullpoint <598757315@qq.com>
2020-04-25 00:30:46 +0800

    Add: 完成 json out 测试

--- test/out_test.go
+++ test/out_test.go
@@ -29,3 +29,17 @@ func TestSQLiteOut(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestJsonOut(t *testing.T) {
+	spider := weint.NewSpider()
+	spider.Uid("5644764907")
+	spider.Type(weint.TYPE_INFO)
+	spider.Type(weint.TYPE_WEIBO)
+	spider.Out(&weint.FileJsonOut{FileOut: weint.FileOut{
+		UserFileName:  "5644764907-user.json",
+		WeiboFileName: "5644764907-weibo.json",
+	}})
+	if err := spider.Run(); err != nil {
+		t.Error(err)
+	}
+}
